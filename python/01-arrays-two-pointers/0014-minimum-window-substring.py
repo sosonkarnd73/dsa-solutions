a07881ba7457f963ff99b2c2c2dce9ff1461d718
Nayan Sonkar <115350797+sosonkarnd73@users.noreply.github.com>
2026-09-21 23:36:03 +0530

    Implement second optimal solution for substring check

--- python/01-arrays-two-pointers/0014-minimum-window-substring.py
+++ python/01-arrays-two-pointers/0014-minimum-window-substring.py
@@ -30,3 +30,41 @@ class Solution:
             i+=1
             j+=1
         return bool(False)
+
+#sol 2
+class Solution:
+    def charactersMap(self, s1: str) -> dict[str, int]:
+        s1charactersmap = {}
+        for i in s1:
+            occ_i = s1charactersmap.get(i, 0)
+            occ_i+=1
+            s1charactersmap[i]=occ_i
+        return s1charactersmap
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1charactersmap = self.charactersMap(s1)
+        l = len(s1)
+        i = 0
+        j = 0
+        windowCharactersMap = {}
+        while(j < len(s2)):
+            c = s2[j]
+            
+            count_c = windowCharactersMap.get(c, 0) + 1
+            windowCharactersMap[c]=count_c
+            count_c_s1 = s1charactersmap.get(c)
+            if not(count_c_s1):
+                windowCharactersMap = {}
+                i = j+1
+                j = j+1
+            elif count_c > count_c_s1:
+                while count_c > count_c_s1:
+                    windowCharactersMap[s2[i]] = windowCharactersMap[s2[i]] - 1
+                    if s2[i] == c:
+                        count_c-=1
+                    i+=1
+                j = j+1
+            elif count_c == count_c_s1 and (j - i +1) == l:
+                return bool(True)
+            else:
+                j+=1
+        return bool(False)
